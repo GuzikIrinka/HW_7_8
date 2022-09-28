254640d6514ee6251951664c83349b3b13475cb4
Irina <andrey_k.90@mail.ru>
2022-09-28 23:07:07 +0300

    imp or exp

---
+++ controller.py
@@ -0,0 +1,16 @@
+from distutils.log import info
+from statistics import mode
+import inp
+import imp
+import exp
+import logg
+
+def start():
+    if inp.mod() == '1':
+        info = inp.exp()
+        exp.expp(info)
+        logg.log_info(info)
+    else:
+        info = inp.inpp()
+        imp.impo(info)
+        logg.log_info(info)
