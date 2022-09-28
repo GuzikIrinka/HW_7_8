3272d876d536c359a805bbeebc515776a818cbb7
Irina <andrey_k.90@mail.ru>
2022-09-28 23:01:58 +0300

    ввод

---
+++ inp.py
@@ -0,0 +1,11 @@
+def mod():
+    mod = input('Выберите необходимый режим. 1 - экспорт, 2 - импорт:')
+    return mod
+
+def inpp():
+    str_person = input('Введите фамилию и номер телефона: ')
+    return str_person
+
+def exp():
+    second_name = input('Введите фамилию: ')
+    return second_name
